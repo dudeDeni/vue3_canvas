1e80400d4559afe5d00f2e5a42fd8f6b22ac666c
dudeDeni <daniillatt@gmail.com>
2021-10-06 00:57:54 +0300

    add draging

--- src/components/UI/CanvasWindow.vue
+++ src/components/UI/CanvasWindow.vue
@@ -9,11 +9,10 @@
 </template>
 
 <script>
-import { ref, onMounted, watch, watchEffect, computed } from "vue";
-import useMakeDragable from "@/hooks/useMakeDragable";
-import useImageLoader from "@/hooks/useImageLoader";
+import { ref, reactive, onMounted, watch, watchEffect, computed } from "vue";
 import Roads from "@/assets/roads.jpg";
 import CarTopView from "@/assets/CarTopView.svg";
+import BlueCar from "@/assets/blue_car.png";
 
 
 export default {
@@ -26,9 +25,8 @@ export default {
     },
     setup(props) {
         const el = ref(null);
-        const {position} = useMakeDragable(el);
-        const {} = useImageLoader(el);
         const carVisible =  ref();
+        let raf;
 
         const renderCar = async () => {
             const canvas = el.value;
@@ -55,18 +53,104 @@ export default {
                 };
             }) 
         }
+        ///////// Draging logic ////////////
+        const position = reactive({
+            init: false,
+            x: 0,
+            y: 0,
+            width: 0,
+            height: 0,
+            isDragging: false,
+            dragStartX: null,
+            dragStartY: null,
+        });
+
+        const drawCar = () => {
+            const ctx = el.value.getContext("2d");
+            ctx.imageSmoothingEnabled = false;
+            const car = new Image();
+            car.src = BlueCar;
+            ctx.beginPath();
+            car.onload = function() {
+                ctx.drawImage(car, position.x, position.y, 60, 50);
+            };
+            ctx.closePath();
+        };
+
+        const renderImg = () => {
+            const canvas = el.value;
+            const ctx = canvas.getContext("2d");
+            renderBackground();
+            ctx.save()
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.restore();
+            drawCar();
+            
+        };
+        
+        const onMouseDown = e => {
+            e.stopPropagation();
+            let {clientX, clientY } = e;
+            position.dragStartX = clientX - position.x;
+            position.dragStartY = clientY - position.y;
+
+            position.isDragging = true;
+
+            document.addEventListener("mouseup", onMouseUp);
+            document.addEventListener("mousemove", onMouseMove);
+            
+        };
+        const onMouseMove = e => {
+            let { clientX, clientY } = e;
+            position.x = clientX - position.dragStartX;
+            position.y = clientY - position.dragStartY;
+            raf = window.requestAnimationFrame(renderImg);
+        };
+        const onMouseUp = e => {
+            position.isDragging = false;
+            position.dragStartX = null;
+            position.dragStartY = null;
+
+            document.removeEventListener("mouseup", onMouseUp);
+            document.removeEventListener("mousemove", onMouseMove);
+            window.cancelAnimationFrame(raf);
+        };
+
+        onMounted(() => {
+            drawCar()
+        })
+
+        watch(el, (el, prevElement, onCleanup) => {
+            document.addEventListener("mousedown", onMouseDown);
+
+            onCleanup(() => {
+                document.removeEventListener("mouseup", onMouseUp);
+                document.removeEventListener("mousemove", onMouseMove);
+                if (el instanceof HTMLElement) {
+                    document.removeEventListener("mousedown", onMouseDown);
+                }
+            })
+        });
+
+        watch(position, (position, prevPos) => {
+            if(position){
+                
+            }
+        })
 
         watch(el, async () => {
             if(el.value){
                 await renderBackground();
+                
             }
         })
+        
         watch(
             () => props.carVisible,
             (carVisible, prevCarVisible) => {
                 console.log("watcher: ", carVisible, prevCarVisible)
                 if(props.carVisible) {
-                    renderCar()
+                    drawCar();
                 }else {
                     renderBackground()
                 }
